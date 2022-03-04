47c7a04ded5d7a397111756efbf128a378008c16
BhumiRaval12 <bhumir@zignuts.com>
2022-03-04 15:32:11 +0530

    updated

--- exp_mngr/ExpenseManager_8/api/controllers/user/register.js
+++ exp_mngr/ExpenseManager_8/api/controllers/user/register.js
@@ -8,19 +8,57 @@ module.exports = {
 
 
   inputs: {
-
+    fullName: {
+      type: 'string',
+      required: true,
+    },
+    email: {
+      type: 'string',
+      required: true,
+      unique: true,
+      isEmail: true,
+    },
+    password: {
+      type: 'string',
+      required: true,
+      minLength: 7,
+    },
   },
 
 
   exits: {
+    success: {
+      statusCode: 201,
+      description: 'New user created',
+    },
+    emailAlreadyInUse: {
+      statusCode: 400,
+      description: 'Email already in use!',
+    },
+    error: {
+      description: 'Something went wrong, try again',
+    },
 
   },
 
 
   fn: async function (inputs) {
-
     // All done.
-    return;
+    try {
+      const newEmailAddress = inputs.email.toLowerCase();
+      let newUser = await User.create({
+        fullName: inputs.fullName,
+        email: newEmailAddress,
+        password: inputs.password,
+      });
+    } catch (error) {
+      if (error.code === 'E_UNIQUE') {
+        return exits.emailAlreadyInUse({
+          message: 'Oops :) an error occurred',
+          error: 'This email already exits',
+        });
+      }
+    }
 
   }
 
--- exp_mngr/ExpenseManager_8/api/models/User.js
+++ exp_mngr/ExpenseManager_8/api/models/User.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 bcrypt = require('bcrypt');
-SALT_WORK_FACTOR = 10;
+
+
+
 
 var UserSchema = new mongoose.Schema({
   fullName: {
@@ -20,59 +22,5 @@ var UserSchema = new mongoose.Schema({
   timestamps: true
 });
 
-UserSchema.pre('save', function (next) {
-  var user = this;
-  // only hash the password if it has been modified (or is new)
-  if (!user.isModified('password')) {
-    return next();
-  }
-  // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-    if (err) {
-      return next(err);
-    }
-    // hash the password using our new salt
-    bcrypt.hash(user.password, salt, (err, hash) => {
-      if (err) {
-        return next(err);
-      }
-      // override the cleartext password with the hashed one
-      user.password = hash;
-      next();
-    });
-  });
-});
-
-UserSchema.methods.comparePassword = function (candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    if (err) {
-      return cb(err);
-    }
-    cb(null, isMatch);
-  });
-};
 
 module.exports = mongoose.model('User', UserSchema);
-
-UserSchema.pre('save', function (next) {
-  var user = this;
-  // only hash the password if it has been modified (or is new)
-  if (!user.isModified('password')) {
-    return next();
-  }
-  // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-    if (err) {
-      return next(err);
-    }
-    // hash the password using our new salt
-    bcrypt.hash(user.password, salt, (err, hash) => {
-      if (err) {
-        return next(err);
-      }
-      // override the cleartext password with the hashed one
-      user.password = hash;
-      next();
-    });
-  });
-});
