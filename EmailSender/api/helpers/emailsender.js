9d7d8c22f2a1dc9ff9a71a1cdda1ef7c8a8c06f1
BhumiRaval12 <bhumir@zignuts.com>
2022-06-22 18:08:01 +0530

    improvement in emailsending helper done

--- EmailSender/api/helpers/emailsender.js
+++ EmailSender/api/helpers/emailsender.js
@@ -1,9 +1,7 @@
 const Role = sails.config.constant.Role;
 module.exports = {
   friendlyName: "Email sender",
-
   description: "Sends an email",
-
   inputs: {
     subject: {
       friendlyName: "Email Subject",
@@ -11,20 +9,16 @@ module.exports = {
       type: "string",
     },
   },
-
   exits: {
     success: {
       description: "All done.",
     },
-
     mailError: {
       description: "Mail could not be sent due to some errors.",
     },
   },
-
   fn: async function (inputs, exits) {
     const nodemailer = require("nodemailer");
-
     const smtpTransport = nodemailer.createTransport({
       service: "gmail",
       host: "smtp.gmail.com",
@@ -33,30 +27,20 @@ module.exports = {
         pass: Role.pass,
       },
     });
-    const maillist = [
-      "bhumiraval456@gmail.com",
-      "bhumir@zignuts.com",
-      "ravirajc@zignuts.com",
-    ];
-    maillist.forEach(async (mail) => {
-      const mailOptions = {
-        from: "bhumiraval875@gmail.com",
-        to: mail,
-        subject: inputs.subject,
-      };
-  
-      await smtpTransport.sendMail(mailOptions, async (err) => {
-        if (err) {
-          console.log(`hii error from mailSender : ${err}`);
-          sails.log.info("error---->", { err });
-          return exits.mailError(err);
-        }
-
-        console.log("hii solved");
-
-        await Email.create(mailOptions);
-      });
+    const mailOptions = {
+      to: "bhumiraval456@gmail.com,bhumir@zignuts.com,ravirajc@zignuts.com",
+      from: "bhumiraval875@gmail.com",
+      subject: inputs.subject,
+    };
+    await smtpTransport.sendMail(mailOptions, async (err) => {
+      if (err) {
+        console.log(`hii error from mailSender : ${err}`);
+        sails.log.info("error---->", { err });
+        return exits.mailError(err);
+      }
+      console.log("hii solved");
+      await Email.create(mailOptions);
+      return exits.success(true);
     });
-    return exits.success(true);
   },
 };
